4ac42a69f81cc69ef494da245938783db26ae65c
Frey44E <valentinsjk@gmail.com>
2025-09-10 10:12:27 +0300

    2st

--- index1.php
+++ index1.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="lv">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>git projekts</title>
+    <title>CRUD projekts</title>
 </head>
 <body>
-    <h1>projekts</h1>
+    <h1>Mans pirmais Git projekts</h1>
 </body>
 </html>
